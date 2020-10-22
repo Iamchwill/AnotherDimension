9b3b85b6cbb564ae0a6a4c360e0b43360d28e988
A2c4u6t <wchen20@stuy.edu>
2020-10-22 14:24:22 -0400

    Made the java files and made the sum method

---
+++ Tester.java
@@ -0,0 +1,9 @@
+public class Tester{
+  public static void main(String[] args) {
+    int[][]  A  =  {  {  1,  0, 12, -1 },
+                      {  7, -2,  2,  1 },
+                      { -5, -2,  2, -9 }  };
+    int[]  B  =   {  2, 4, 4 };
+    System.out.println(ArrayOps.sum(B));
+  }
+}
